14755d26f5507ede88834721ecfcd13375e99894
Rob <robertchristopherdev11@gmail.com>
2016-11-07 20:40:19 -1000

    Added gunicorn deploy script

--- bin/gunicorn.sh
+++ bin/gunicorn.sh
@@ -2,13 +2,14 @@
 
 NAME="tbhapi"
 APPDIR=$APPDIR
-PORT=$PORT
-NUM_WORKERS=$WORKERS
 
 echo "Starting $NAME"
 
 # Start your gunicorn
-exec gunicorn tbh:app -b 0.0.0.0:PORT \
+exec gunicorn run -b 0.0.0.0:$PORT \
   --name $NAME \
   --workers $NUM_WORKERS \
   --user=$USER --group=$GROUP \
+
+gunicorn run:application -b 0.0.0.0:$PORT   --name $NAME   --workers $NUM_WORKERS
+gunicorn -k tornado -b 0.0.0.0:$PORT -w 4 run:application
