83b70ba535c7e56a7b4dd0492b4372359fe3e245
Rob <robertchristopherdev11@gmail.com>
2016-11-07 20:36:29 -1000

    Run options

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
-from app import main
+from app import (main, start_development_server)
 import tornado.httpserver
 
 
 application = main()
-tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == '__main__':
+    app = start_development_server()
+    tornado.ioloop.IOLoop.instance().start()
