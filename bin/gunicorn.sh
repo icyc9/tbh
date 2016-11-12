f6f2f730e97b6ac52146e0d22363438b8415da02
Rob <robertchristopherdev11@gmail.com>
2016-11-12 11:08:52 -0800

    Added supervisor

--- bin/gunicorn.sh
+++ bin/gunicorn.sh
@@ -2,14 +2,15 @@
 
 NAME="tbhapi"
 APPDIR=$APPDIR
+PORT=8082
 
 echo "Starting $NAME"
 
 # Start your gunicorn
-exec gunicorn run -b 0.0.0.0:$PORT \
-  --name $NAME \
-  --workers $NUM_WORKERS \
-  --user=$USER --group=$GROUP \
+#exec gunicorn run -b 0.0.0.0:$PORT \
+#  --name $NAME \
+#  --workers $NUM_WORKERS \
+#  --user=$USER --group=$GROUP \
 
-gunicorn run:application -b 0.0.0.0:$PORT   --name $NAME   --workers $NUM_WORKERS
+#gunicorn run:application -b 0.0.0.0:$PORT   --name $NAME   --workers $NUM_WORKERS
 gunicorn -k tornado -b 0.0.0.0:$PORT -w 4 run:application
